c8e64c3fb6ab79f19442fe400310b8bc5e659b26
justvanrossum <justvanrossum@gmail.com>
2021-04-16 15:33:37 +0200

    setup.py, beginnings of test suite

---
+++ Tests/test_blackrenderer.py
@@ -0,0 +1,4 @@
+def test_import():
+    import blackrenderer
+
+    assert blackrenderer.__version__ != "<unknown>"
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="blackrenderer",
+    use_scm_version={"write_to": "Lib/blackrenderer/_version.py"},
+    python_requires=">=3.7",
+    package_dir={"": "Lib"},
+    packages=find_packages("Lib"),
+    install_requires=[
+        "fonttools >= 4.17.0",
+    ],
+    setup_requires=["setuptools_scm"],
+)
