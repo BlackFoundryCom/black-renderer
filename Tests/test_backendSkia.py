877b5b9f48d45610bb0ade198f7401288da1e6ae
justvanrossum <justvanrossum@gmail.com>
2021-04-25 09:41:31 +0200

    initial test of Skia backend

---
+++ Tests/test_backendSkia.py
@@ -0,0 +1,29 @@
+import pathlib
+import pytest
+from blackrenderer.colrFont import COLRFont
+from blackrenderer.backendSkia import PixelSurface, SkiaBackend
+
+
+testDir = pathlib.Path(__file__).resolve().parent
+dataDir = testDir / "data"
+tmpOutputDir = testDir / "tmpOutput"
+if not tmpOutputDir.exists():
+    tmpOutputDir.mkdir()
+
+
+testFont1 = dataDir / "noto-glyf_colr_1.ttf"
+testFont2 = dataDir / "samples-glyf_colr_1.ttf"
+
+
+@pytest.mark.parametrize("glyphName", ["uni2693", "uni2694"])
+def test_renderGlyph(glyphName):
+    font = COLRFont(testFont1)
+
+    minX, minY, maxX, maxY = font.getGlyphBounds(glyphName)
+
+    surface = PixelSurface(minX, minY, maxX - minX, maxY - minY)
+    backend = SkiaBackend(surface.canvas)
+
+    font.drawGlyph(glyphName, backend)
+
+    surface.saveImage(tmpOutputDir / f"{glyphName}.png")
