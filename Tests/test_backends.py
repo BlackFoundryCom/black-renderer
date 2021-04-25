670eeb66a185bac339af97e00d188436ee9a80b6
justvanrossum <justvanrossum@gmail.com>
2021-04-25 15:32:46 +0200

    parametrize the backend itself, so we run all tests on all backends

---
+++ Tests/test_backends.py
@@ -0,0 +1,35 @@
+import pathlib
+import pytest
+from blackrenderer.colrFont import COLRFont
+from blackrenderer.backendCairo import CairoPixelSurface
+from blackrenderer.backendSkia import SkiaPixelSurface
+
+
+surfaceFactories = {
+    "cairo": CairoPixelSurface,
+    "skia": SkiaPixelSurface,
+}
+
+testDir = pathlib.Path(__file__).resolve().parent
+dataDir = testDir / "data"
+tmpOutputDir = testDir / "tmpOutput"
+if not tmpOutputDir.exists():
+    tmpOutputDir.mkdir()
+
+
+testFont1 = dataDir / "noto-glyf_colr_1.ttf"
+testFont2 = dataDir / "samples-glyf_colr_1.ttf"
+
+
+@pytest.mark.parametrize("surfaceFactoryName", surfaceFactories.keys())
+@pytest.mark.parametrize("glyphName", ["uni2693", "uni2694"])
+def test_renderGlyph(surfaceFactoryName, glyphName):
+    font = COLRFont(testFont1)
+
+    minX, minY, maxX, maxY = font.getGlyphBounds(glyphName)
+
+    surfaceFactory = surfaceFactories[surfaceFactoryName]
+    surface = surfaceFactory(minX, minY, maxX - minX, maxY - minY)
+    font.drawGlyph(glyphName, surface.backend)
+
+    surface.saveImage(tmpOutputDir / f"{surfaceFactoryName}_{glyphName}.png")
