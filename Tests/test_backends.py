e9d28b18881454fa157cbd28f1ad14b49385f82f
justvanrossum <justvanrossum@gmail.com>
2021-04-25 16:06:11 +0200

    simplify

--- Tests/test_backends.py
+++ Tests/test_backends.py
@@ -16,21 +16,20 @@ testFont1 = dataDir / "noto-glyf_colr_1.ttf"
 testFont2 = dataDir / "samples-glyf_colr_1.ttf"
 
 
-surfaceFactories = {
-    "cairo": CairoPixelSurface,
-    "skia": SkiaPixelSurface,
-}
+backends = [
+    ("cairo", CairoPixelSurface),
+    ("skia", SkiaPixelSurface),
+]
 
 
-@pytest.mark.parametrize("surfaceFactoryName", surfaceFactories.keys())
+@pytest.mark.parametrize("backendName, surfaceFactory", backends)
 @pytest.mark.parametrize("glyphName", ["uni2693", "uni2694"])
-def test_renderGlyph(surfaceFactoryName, glyphName):
+def test_renderGlyph(backendName, surfaceFactory, glyphName):
     font = COLRFont(testFont1)
 
     minX, minY, maxX, maxY = font.getGlyphBounds(glyphName)
 
-    surfaceFactory = surfaceFactories[surfaceFactoryName]
     surface = surfaceFactory(minX, minY, maxX - minX, maxY - minY)
     font.drawGlyph(glyphName, surface.backend)
 
-    surface.saveImage(tmpOutputDir / f"{surfaceFactoryName}_{glyphName}.png")
+    surface.saveImage(tmpOutputDir / f"{backendName}_{glyphName}.png")
