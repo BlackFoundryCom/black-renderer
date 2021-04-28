04d8f9ec99885c599ed194fb4222af4cb5d0eeaa
justvanrossum <justvanrossum@gmail.com>
2021-04-28 07:31:16 +0200

    new backends submodule: backends will move there

---
+++ Lib/blackrenderer/backends/base.py
@@ -0,0 +1,60 @@
+from abc import ABC, abstractmethod
+
+
+class Backend(ABC):
+    @abstractmethod
+    def __init__(self, context):
+        ...
+
+    @staticmethod
+    @abstractmethod
+    def newPath():
+        ...
+
+    @abstractmethod
+    @contextmanager
+    def savedState(self):
+        ...
+
+    @abstractmethod
+    def transform(self, transform):
+        ...
+
+    @abstractmethod
+    def clipPath(self, path):
+        ...
+
+    @abstractmethod
+    def fillSolid(self, color):
+        ...
+
+    @abstractmethod
+    def fillLinearGradient(self, colorLine, pt1, pt2, extendMode):
+        ...
+
+    @abstractmethod
+    def fillRadialGradient(
+        self, colorLine, startCenter, startRadius, endCenter, endRadius, extendMode
+    ):
+        ...
+
+    @abstractmethod
+    def fillSweepGradient(self, *args):
+        ...
+
+
+class Surface(ABC):
+    fileExtension = ".png"
+
+    @abstractmethod
+    def __init__(self, x, y, width, height):
+        ...
+
+    @property
+    @abstractmethod
+    def backend(self):
+        ...
+
+    @abstractmethod
+    def saveImage(self, path):
+        ...
