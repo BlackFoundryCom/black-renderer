405f7af8835ea418a8b9af8fa1836dee3bed4677
justvanrossum <justvanrossum@gmail.com>
2021-04-29 09:37:38 +0200

    these tests are no longer needed, as the canvas should only be called with a [0,1] normalized colorline

--- Tests/test_backends.py
+++ Tests/test_backends.py
@@ -49,10 +49,6 @@ def test_renderGlyph(backendName, surfaceFactory, fontName, glyphName):
 
 test_colorStops = [
     (0, 1),
-    (0.25, 0.75),
-    (0.25, 1.5),
-    (-0.5, 0.75),
-    (1, 0),
 ]
 
 test_extendModes = [ExtendMode.PAD, ExtendMode.REPEAT, ExtendMode.REFLECT]
