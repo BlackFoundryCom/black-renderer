4ecda841cc50b7377eded2a4ec0a7916959768c7
justvanrossum <justvanrossum@gmail.com>
2021-05-28 23:01:32 +0200

    increase error margin

--- Tests/test_canvas_api.py
+++ Tests/test_canvas_api.py
@@ -133,4 +133,4 @@ def test_compositeMode(backendName, surfaceFactory, compositeMode):
     outputPath = tmpOutputDir / fileName
     surface.saveImage(outputPath)
     diff = compareImages(expectedPath, outputPath)
-    assert diff < 0.0001, diff
+    assert diff < 0.00013, diff
