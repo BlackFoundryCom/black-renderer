c4c7f8311755fa1efcd90f263f6d0b2200e42358
justvanrossum <justvanrossum@gmail.com>
2021-05-30 19:38:47 +0200

    add optional imageExtension argument to getSurfaceClass(), and add the new PDF and SVG surfaces to the _surfaces table

--- Lib/blackrenderer/backends/__init__.py
+++ Lib/blackrenderer/backends/__init__.py
@@ -2,15 +2,32 @@ import importlib
 
 
 _surfaces = {
-    "cairo": "blackrenderer.backends.cairo.CairoPixelSurface",
-    "coregraphics": "blackrenderer.backends.coregraphics.CoreGraphicsPixelSurface",
-    "skia": "blackrenderer.backends.skia.SkiaPixelSurface",
-    "svg": "blackrenderer.backends.svg.SVGSurface",
+    None: {
+        "cairo": "blackrenderer.backends.cairo.CairoPixelSurface",
+        "coregraphics": "blackrenderer.backends.coregraphics.CoreGraphicsPixelSurface",
+        "skia": "blackrenderer.backends.skia.SkiaPixelSurface",
+        "svg": "blackrenderer.backends.svg.SVGSurface",
+    },
+    ".png": {
+        "cairo": "blackrenderer.backends.cairo.CairoPixelSurface",
+        "coregraphics": "blackrenderer.backends.coregraphics.CoreGraphicsPixelSurface",
+        "skia": "blackrenderer.backends.skia.SkiaPixelSurface",
+    },
+    ".pdf": {
+        "cairo": "blackrenderer.backends.cairo.CairoPDFSurface",
+        "coregraphics": "blackrenderer.backends.coregraphics.CoreGraphicsPDFSurface",
+        "skia": "blackrenderer.backends.skia.SkiaPDFSurface",
+    },
+    ".svg": {
+        "cairo": "blackrenderer.backends.cairo.CairoSVGSurface",
+        "skia": "blackrenderer.backends.skia.SkiaSVGSurface",
+        "svg": "blackrenderer.backends.svg.SVGSurface",
+    },
 }
 
 
-def getSurfaceClass(name):
-    fqName = _surfaces[name]
+def getSurfaceClass(name, imageExtension=None):
+    fqName = _surfaces[imageExtension][name]
     moduleName, className = fqName.rsplit(".", 1)
     try:
         module = importlib.import_module(moduleName)
